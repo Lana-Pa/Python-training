62ec9890b8b9471ccb3b3872a496e736fc7aaa14
Lana Paulikava <pavlikova.lana@gmail.com>
2016-12-13 10:55:57 -0800

    Выделены вспомогательные методы и классы

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+from group import Group
+import unittest
+
 
 def is_alert_present(wd):
     try:
@@ -14,34 +15,69 @@ class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
-    def test_test_add_group(self):
-        success = True
-        wd = self.wd
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_groups(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def init_group_creation(self, wd):
         wd.find_element_by_name("new").click()
+
+    def fill_group_form(self, wd, Group):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("zdfaxzsx")
+        wd.find_element_by_name("group_name").send_keys(Group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("dsfszfeew")
+        wd.find_element_by_name("group_header").send_keys(Group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("dsfwefawerw")
+        wd.find_element_by_name("group_footer").send_keys(Group.footer)
+
+    def submit_group_creation(self, wd):
         wd.find_element_by_name("submit").click()
+
+    def return_to_group(self, wd):
         wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
+    #Test Login and create a group-------------------------------------
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username = "admin", password = "secret")
+        self.open_groups(wd)
+        self.init_group_creation(wd)
+        self.fill_group_form(wd, Group(name = "My group", header = "Headername", footer = "Footername"))
+        self.submit_group_creation(wd)
+        self.return_to_group(wd)
+        self.logout(wd)
+
+    # Test Check for empty groups-------------------------------------
+    def test_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username = "admin", password = "secret")
+        self.open_groups(wd)
+        self.init_group_creation(wd)
+        self.fill_group_form(wd, Group(name = "", header = "", footer = ""))
+        self.submit_group_creation(wd)
+        self.return_to_group(wd)
+        self.logout(wd)
+
     def tearDown(self):
         self.wd.quit()
 
