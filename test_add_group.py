38f5c33a4d55b129116df9ac237914559fdafd38
Lana Paulikava <pavlikova.lana@gmail.com>
2016-12-15 12:04:01 -0800

    Доработка тестов и вспомагательных функций

--- test_add_group.py
+++ test_add_group.py
@@ -20,6 +20,7 @@ class test_add_group(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -28,13 +29,12 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def open_groups(self, wd):
+    def create_group(self, wd, Group):
+        #open group page
         wd.find_element_by_link_text("groups").click()
-
-    def init_group_creation(self, wd):
+        #init group creation
         wd.find_element_by_name("new").click()
-
-    def fill_group_form(self, wd, Group):
+        #fill out form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(Group.name)
@@ -44,9 +44,9 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(Group.footer)
-
-    def submit_group_creation(self, wd):
         wd.find_element_by_name("submit").click()
+        #return to group page
+        self.return_to_group(wd)
 
     def return_to_group(self, wd):
         wd.find_element_by_link_text("group page").click()
@@ -54,28 +54,18 @@ class test_add_group(unittest.TestCase):
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    #Test Login and create a group-------------------------------------
+    #Test Create a group-------------------------------------
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username = "admin", password = "secret")
-        self.open_groups(wd)
-        self.init_group_creation(wd)
-        self.fill_group_form(wd, Group(name = "My group", header = "Headername", footer = "Footername"))
-        self.submit_group_creation(wd)
-        self.return_to_group(wd)
+        self.create_group(wd, Group(name = "My group", header = "Headername", footer = "Footername"))
         self.logout(wd)
 
-    # Test Check for empty groups-------------------------------------
+    # Test Create an empty group-------------------------------------
     def test_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username = "admin", password = "secret")
-        self.open_groups(wd)
-        self.init_group_creation(wd)
-        self.fill_group_form(wd, Group(name = "", header = "", footer = ""))
-        self.submit_group_creation(wd)
-        self.return_to_group(wd)
+        self.create_group(wd, Group(name = "", header = "", footer = ""))
         self.logout(wd)
 
     def tearDown(self):
