182b2c73786420cae7651aeccd9dace038487fbe
Lana Paulikava <pavlikova.lana@gmail.com>
2016-12-27 12:36:12 -0800

    Проверка наличия контакта перед удалением/модификацией

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -1,21 +1,7 @@
 from model.contact import Contact
 
-def test_delete_first_contact(app):  #add a fixture app as a parameter
+
+def test_delete_first_contact(app):  # add a fixture app as a parameter
     if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="Ivan",
-                                   middlename="Stepanovich",
-                                   lastname="Ivanov",
-                                   nickname="Van",
-                                   title="Mr",
-                                   company="Home",
-                                   address="pr. Mira 55-188, Moscow, Russia",
-                                   home_phone="111222555",
-                                   mobile_phone="922555666",
-                                   work_phone="822545654",
-                                   email1="p1@co.com",
-                                   email2="p2@co.com",
-                                   email3="p3@co.com",
-                                   homepage="www.mypage.com",
-                                   address2="ul.Lenina 22-25, Moscow, Russia",
-                                   phone2="456254856"))
+        app.contact.create(Contact(firstname="Ivan"))
     app.contact.delete_first_contact()
