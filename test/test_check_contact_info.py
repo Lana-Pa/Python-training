17e591ac6e46aaddd874210613143ea4d3d25d12
Lana Paulikava <pavlikova.lana@gmail.com>
2017-01-20 09:18:04 -0800

    Тесты для проверки инфо контакта сгруппированы в один файл
    test_check_contact_info.py

--- test/test_check_contact_info.py
+++ test/test_check_contact_info.py
@@ -1,25 +1,41 @@
 import re
 
+# Compate phones on home page with edit page
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
+# Compate phones on view page with edit page
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert merge_phones_like_on_home_page(contact_from_view_page) == merge_phones_like_on_home_page(contact_from_edit_page)
 
-    # assert contact_from_view_page.home_phone == contact_from_edit_page.home_phone
-    # assert contact_from_view_page.mobile_phone == contact_from_edit_page.mobile_phone
-    # assert contact_from_view_page.work_phone == contact_from_edit_page.work_phone
-    # assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
-
+# delete all unnecessary symbols
 def clear(s):
     return re.sub("[() -]", "", s)
 
+# merge phones from edit page like on home page
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x!= "",  # get only non-empty rows, delete empty
                             map(lambda x: clear(x),  # apply function clear to all list elements
                                  filter(lambda x: x is not None,  # delete all empty phones
                                         [contact.home_phone, contact.mobile_phone, contact.work_phone, contact.phone2]))))
+
+# Compate address on home page with edit page
+def test_address_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.address == contact_from_edit_page.address
+
+# Compate emails on home page with edit page
+def test_emails_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x!= "",  # get only non-empty rows, delete empty
+                              filter(lambda x: x is not None,  # delete all empty emails
+                                [contact.email1, contact.email2, contact.email3])))
