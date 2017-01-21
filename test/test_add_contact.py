026faf83846f944dbed41ccba8966114e37b75f0
Lana Paulikava <pavlikova.lana@gmail.com>
2017-01-21 12:22:43 -0800

    Добавлена параметризация тестов для групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,34 +1,43 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
+import pytest
+import random
+import string
 
-# TEST add a contact
-def test_add_contact(app):
+# generate random string---------------------------------------------------------------------
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+# generate test data
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
+                    address="", home_phone="", mobile_phone="",work_phone="", email1="",email2="", email3="",
+                    homepage="", address2="", phone2="")]+[
+            Contact(firstname=random_string("firstname",10), middlename=random_string("middlename",10),
+                    lastname=random_string("lastname",10), nickname=random_string("nickname",10),
+                    title=random_string("title",10), company=random_string("company",10),
+                    address=random_string("address",15), home_phone=random_string("homephone",10),
+                    mobile_phone=random_string("mobilephone",10),work_phone=random_string("workphone",10),
+                    email1=random_string("email1",10),email2=random_string("email2",10),
+                    email3=random_string("email3",10), homepage=random_string("homepage",10), address2=random_string("address2",10),
+                    phone2=random_string("phone2",10))
+            for i in range(5)]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
+
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname ="Ivan",
-                       middlename = "Stepanovich",
-                       lastname = "Ivanov",
-                       nickname = "Van",
-                       title = "Mr",
-                       company = "Home",
-                       address = "pr. Mira 55-188, Moscow, Russia",
-                       home_phone = "111222555",
-                       mobile_phone = "922555666",
-                       work_phone = "822545654",
-                       email1 = "p1@co.com",
-                       email2 = "p2@co.com",
-                       email3 ="p3@co.com",
-                       homepage = "www.mypage.com",
-                       address2 = "ul.Lenina 22-25, Moscow, Russia",
-                       phone2 = "456254856")
     app.contact.create(contact)
+
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-# TEST add an empty contact
+# ------TEST add an empty contact
 # def test_add_empty_contact(app):
 #     old_contacts = app.contact.get_contact_list()
 #     contact = Contact(firstname="",
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
-
 from model.group import Group
+import pytest
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-# Test Create a group-------------------------------------
-def test_add_group(app):  # add a fixture app as a parameter
+testdata = [Group(name="", header="", footer="")]+[
+    Group(name=random_string("name",10), header=random_string("header",20), footer=random_string("footer",20))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):  # add a fixture app as a parameter
     old_groups = app.group.get_group_list() # make a list of groups before adding a new one
-    group = Group(name="My group", header="Headername", footer="Footername")
     app.group.create(group) # add a new group
 
     assert len(old_groups) + 1 == app.group.count()  # compare size of groups list with result of hash-function count()
@@ -14,15 +24,3 @@ def test_add_group(app):  # add a fixture app as a parameter
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-
-
-# Test Create an empty group-------------------------------------
-# def test_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
