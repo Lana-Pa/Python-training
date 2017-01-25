aa8f990f3fbe5b07e56d1be3d25d047fdd85d185
Lana Paulikava <pavlikova.lana@gmail.com>
2017-01-25 12:28:16 -0800

    Добавлено динамическое связывание данных с тестовой функцией add_group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,8 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")]+[
-    Group(name=random_string("name",10), header=random_string("header",20), footer=random_string("footer",20))
-    for i in range(5)
-]
-
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):  # add a fixture app as a parameter
+def test_add_group(app, data_groups):  # add a fixture app as a parameter, data_groups - load data for testing from data/group.py
+    group = data_groups
     old_groups = app.group.get_group_list() # make a list of groups before adding a new one
     app.group.create(group) # add a new group
 
