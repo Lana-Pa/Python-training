cbc5be3c16626404dd69ca4bc88e46ee6573ccd4
Lana Paulikava <pavlikova.lana@gmail.com>
2017-01-26 11:36:34 -0800

    Реализованы генераторы тестовых данных для групп и контактов
    сохраняющие созданные данные в файл в формате JSON.
    Использована библиотека jsonpickle для сохранения структуры объектов в
    формат JSON.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,6 +2,7 @@ from model.contact import Contact
 from random import randrange
 
 def test_modify_contact_first_name(app):  #add a fixture app as a parameter
+
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Ivan", lastname="Fedorov"))
     old_contacts = app.contact.get_contact_list()
@@ -16,11 +17,3 @@ def test_modify_contact_first_name(app):  #add a fixture app as a parameter
     new_contacts = app.contact.get_contact_list()
     old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-# def test_modify_contact_address(app):  #add a fixture app as a parameter
-#     if app.contact.count() == 0:
-#         app.contact.create(Contact(firstname="Ivan"))
-#     old_contacts = app.contact.get_contact_list()
-#     app.contact.modify_first_contact(Contact(address ="Edited address"))
-#     new_contacts = app.contact.get_contact_list()
-#     assert len(old_contacts) == len(new_contacts)
